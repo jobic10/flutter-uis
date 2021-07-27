34446223fc4c28cd32104914444c69e5ff8fe86d
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-07-27 18:02:12 +0100

    UI Complete

--- lib/projects/dating/screens/recommendation_details_screen.dart
+++ lib/projects/dating/screens/recommendation_details_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_uis/projects/dating/widgets/interest_card.dart';
 
 class RecommendationDetailsScreen extends StatelessWidget {
   const RecommendationDetailsScreen({Key? key}) : super(key: key);
@@ -39,71 +40,191 @@ class RecommendationDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
-      children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Align(
-              alignment: Alignment.topRight,
-              child: ClipRRect(
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(50.h),
-                ),
-                child: Image.asset(
-                  'assets/dating/images/rose2.png',
-                  width: 380.w,
-                  height: 412.h,
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.only(
-                top: 46.h,
-                left: 55.w,
-              ),
-              child: RichText(
-                text: TextSpan(
-                  style: TextStyle(
-                    height: 1.4.h,
-                    color: Color(0XFF16123D),
+        body: SingleChildScrollView(
+      child: Stack(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Align(
+                alignment: Alignment.topRight,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(50.h),
                   ),
+                  child: Image.asset(
+                    'assets/dating/images/rose2.png',
+                    width: 380.w,
+                    height: 412.h,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(
+                  top: 46.h,
+                  left: 55.w,
+                ),
+                child: RichText(
+                  text: TextSpan(
+                    style: TextStyle(
+                      height: 1.4.h,
+                      color: Color(0XFF16123D),
+                    ),
+                    children: [
+                      TextSpan(
+                        text: 'Akinola Roseline, 24',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 24.sp,
+                        ),
+                      ),
+                      TextSpan(
+                        text: '  \u{1F7E2} \n',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 10.sp,
+                        ),
+                      ),
+                      TextSpan(
+                        text: 'Full stack developer \n',
+                        style: TextStyle(
+                          fontSize: 16.sp,
+                        ),
+                      ),
+                      TextSpan(
+                        text: '76m away',
+                        style: TextStyle(
+                          fontSize: 12.sp,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(
+                  left: 24.w,
+                  top: 14.h,
+                  right: 24.w,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
-                    TextSpan(
-                      text: 'Malena Victoria, 23',
+                    Text(
+                      'ABOUT ME',
                       style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 24.sp,
+                        fontSize: 18.sp,
+                        fontWeight: FontWeight.w500,
+                        color: Color(0XFF4b4e6f),
                       ),
                     ),
-                    TextSpan(
-                      text: ' \u{1F7E2} \n',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 14.sp,
-                      ),
+                    SizedBox(
+                      height: 10.h,
                     ),
-                    TextSpan(
-                      text: 'Full stack developer \n',
+                    Text(
+                      'Hey guys, this is Rose 🌹. I am here to find someone for hookup. I am not interested in something serious. I would love to hear your adventurous story.',
+                      textAlign: TextAlign.justify,
                       style: TextStyle(
-                        fontSize: 16.sp,
-                      ),
-                    ),
-                    TextSpan(
-                      text: '76m away',
-                      style: TextStyle(
-                        fontSize: 12.sp,
+                        fontSize: 15.sp,
                       ),
                     ),
                   ],
                 ),
               ),
-            )
-          ],
-        ),
-        _appBar(context),
-      ],
+              Container(
+                padding: EdgeInsets.only(
+                  top: 25.h,
+                  left: 24.w,
+                  right: 24.w,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text(
+                      'INTERESTS',
+                      style: TextStyle(
+                        fontSize: 18.sp,
+                        fontWeight: FontWeight.w500,
+                        color: Color(0XFF4b4e6f),
+                      ),
+                    ),
+                    SizedBox(height: 14.h),
+                    Wrap(
+                      spacing: 12.w,
+                      runSpacing: 14.h,
+                      children: [
+                        InterestCard(color: Colors.blue, text: 'Code'),
+                        InterestCard(color: Colors.red, text: 'Dance'),
+                        InterestCard(color: Colors.green, text: 'Reading'),
+                        InterestCard(color: Colors.orange, text: 'Cook'),
+                        InterestCard(color: Colors.grey, text: 'Sing'),
+                        InterestCard(color: Colors.purple, text: 'Travel'),
+                        InterestCard(color: Colors.black, text: 'Soccer'),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(
+                  top: 40.h,
+                  left: 24.w,
+                  right: 24.w,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text(
+                      'INSTAGRAM PHOTOS',
+                      style: TextStyle(
+                        fontSize: 18.sp,
+                        fontWeight: FontWeight.w500,
+                        color: Color(0XFF4b4e6f),
+                      ),
+                    ),
+                    SizedBox(height: 14.h),
+                    Wrap(
+                      spacing: 12.w,
+                      runSpacing: 14.h,
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(8),
+                          child: Image.asset(
+                            'assets/dating/images/rose.png',
+                            width: 100.w,
+                            height: 200.h,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(8),
+                          child: Image.asset(
+                            'assets/dating/images/rose2.png',
+                            width: 100.w,
+                            height: 200.h,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(8),
+                          child: Image.asset(
+                            'assets/dating/images/rose.png',
+                            width: 100.w,
+                            height: 200.h,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+          _appBar(context),
+        ],
+      ),
     ));
   }
 }
