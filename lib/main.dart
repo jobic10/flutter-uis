dca6cb3a69045bfef1bc90fa29fdbcb1c8b60a4f
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-08-02 09:27:26 +0100

    Dating UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_uis/projects/dating/screens/recommendation.dart';
 
+import 'projects/dating/screens/screen.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -29,16 +31,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -51,11 +45,18 @@ class _MyHomePageState extends State<MyHomePage> {
             title: Text('Dating UI'),
             leading: Icon(Icons.favorite),
             onTap: () {
-              print(MediaQuery.of(context).size);
               Navigator.of(context).push(MaterialPageRoute(
                   builder: (context) => RecommendationScreen()));
             },
-          )
+          ),
+          ListTile(
+            title: Text('Delete'),
+            leading: Icon(Icons.favorite),
+            onTap: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => ScreenApp()));
+            },
+          ),
         ],
       ),
     );
--- lib/projects/dating/screens/recommendation.dart
+++ lib/projects/dating/screens/recommendation.dart
@@ -9,83 +9,85 @@ class RecommendationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
-
     print('Width : ${size.width} and Height : ${size.height}');
     return ScreenUtilInit(
       designSize: Size(392.7, 825.5),
       builder: () => Scaffold(
-        body: Column(
-          children: [
-            _appBar(),
-            GestureDetector(
-              onTap: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (_) => RecommendationDetailsScreen()));
-              },
-              child: Stack(
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(20),
-                    child: Image.asset(
-                      'assets/dating/images/rose.png',
-                      width: 366.w,
-                      height: 622.h,
-                      fit: BoxFit.cover,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              _appBar(),
+              SizedBox(height: 20.h),
+              GestureDetector(
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (_) => RecommendationDetailsScreen()));
+                },
+                child: Stack(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Image.asset(
+                        'assets/dating/images/rose.png',
+                        width: 366.w,
+                        height: 622.h,
+                        fit: BoxFit.fill,
+                      ),
                     ),
-                  ),
-                  Positioned(
-                    right: 0,
-                    bottom: 40.h,
-                    child: Container(
-                      width: 342.w,
-                      height: 104.h,
-                      decoration: BoxDecoration(
-                        color: Colors.white.withOpacity(0.9),
-                        borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(20),
-                          topLeft: Radius.circular(20),
+                    Positioned(
+                      right: 0,
+                      bottom: 40.h,
+                      child: Container(
+                        width: 342.w,
+                        height: 104.h,
+                        decoration: BoxDecoration(
+                          color: Colors.white.withOpacity(0.9),
+                          borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(20),
+                            topLeft: Radius.circular(20),
+                          ),
+                        ),
+                        padding: EdgeInsets.only(
+                          left: 22.w,
+                          top: 24.h,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Akinola Roseline, 24',
+                              style: TextStyle(
+                                fontSize: 24.sp,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              'Full Stack Developer',
+                              style: TextStyle(
+                                fontSize: 16.sp,
+                                color: Colors.grey,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                      padding: EdgeInsets.only(
-                        left: 22.w,
-                        top: 24.h,
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Akinola Roseline, 24',
-                            style: TextStyle(
-                              fontSize: 24.sp,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            'Full Stack Developer',
-                            style: TextStyle(
-                              fontSize: 16.sp,
-                              color: Colors.grey,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  )
-                ],
+                    )
+                  ],
+                ),
               ),
-            ),
-            Container(
-              height: 100.h,
-              child: BottomNav(
-                height: 64.h,
-                width: 248.w,
-                size1: 54.w,
-                size2: 50.w,
-                size3: 54.w,
-              ),
-            )
-          ],
+              Container(
+                height: 100.h,
+                child: BottomNav(
+                  height: 64.h,
+                  width: 248.w,
+                  size1: 54.w,
+                  size2: 50.w,
+                  size3: 54.w,
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
@@ -103,11 +105,13 @@ class RecommendationScreen extends StatelessWidget {
         height: 34.h,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Image.asset(
               'assets/dating/images/user.png',
               height: 24.h,
               width: 24.w,
+              fit: BoxFit.fill,
             ),
             Text(
               'For You',
